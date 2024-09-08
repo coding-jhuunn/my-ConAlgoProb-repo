7a79c3ac2e0e53de12987ef511a4d2d1fb72bb42
coding-jhuunn <codingatjhunn@gmail.com>
2024-09-08 21:03:33 +0800

    arrange files

---
+++ Problems/sumArray.js
@@ -0,0 +1,10 @@
+const exampleArr = [2, 5, 7, 1, 23, 23];
+// Calculate the sum of numbers within an array
+function sumArray(givenArr) {
+  let sum = 0;
+  for (let index = 0; index < givenArr.length; index++) {
+    sum += givenArr[index];
+  }
+  return sum;
+}
+//console.log(sumArray(exampleArr));
--- Problems/sumofRowAndCol.js
+++ Problems/sumofRowAndCol.js
@@ -1,14 +1,3 @@
-const exampleArr = [2, 5, 7, 1, 23, 23];
-// Calculate the sum of numbers within an array
-function sumArray(givenArr) {
-  let sum = 0;
-  for (let index = 0; index < givenArr.length; index++) {
-    sum += givenArr[index];
-  }
-  return sum;
-}
-//console.log(sumArray(exampleArr));
-
 //calculate specific row or col in a 2d Array
 let MathScore = [
   [4, 20, 60, 1],
@@ -40,5 +29,5 @@ function sumOfCol(givenArr, specCol) {
   return sum;
 }
 
-console.log(sumOfRow(MathScore, 3));
-console.log(sumOfCol(MathScore, 0));
+// console.log(sumOfRow(MathScore, 3));
+// console.log(sumOfCol(MathScore, 0));
