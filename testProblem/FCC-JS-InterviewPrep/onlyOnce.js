f4f291ca82c25ec91a4d4598674f3027de912a57
coding-jhuunn <codingatjhunn@gmail.com>
2024-12-11 21:10:19 +0800

    onlyOncePart 2 added

--- testProblem/FCC-JS-InterviewPrep/onlyOnce.js
+++ testProblem/FCC-JS-InterviewPrep/onlyOnce.js
@@ -17,3 +17,22 @@ let isSubscribed = likeVideo();
 isSubscribed();
 isSubscribed();
 isSubscribed();
+
+//or like this
+
+function once(func, context) {
+  let ran;
+  return function () {
+    if (func) {
+      ran = func.apply(context || this, arguments);
+      func = null;
+    }
+    return ran;
+  };
+}
+
+const hello = once((a, b) => console.log(a, b));
+
+hello(1, 2);
+hello(1, 2);
+hello(1, 2);
