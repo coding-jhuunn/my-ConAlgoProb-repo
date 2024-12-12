82fd67fdf36e14b16af19dd6cd36743d1aa2cc78
coding-jhuunn <codingatjhunn@gmail.com>
2024-12-12 21:26:59 +0800

    q1 currying added

--- testProblem/FCC-JS-InterviewPrep/currying.js
+++ testProblem/FCC-JS-InterviewPrep/currying.js
@@ -5,3 +5,16 @@ function f(a) {
 }
 
 console.log(f(5)(6));
+
+/// another example of currying in js
+
+// question 1 - sum(2)(6)(1)
+function sum(a) {
+  return function (b) {
+    return function (c) {
+      return a + b + c;
+    };
+  };
+}
+
+console.log(sum(2)(6)(3));
